253eff0d13cfb971f2e963f67737f2bb212bc90c
Clinton Guenther <clinton.guenther@gmail.com>
2019-10-21 12:13:58 -0500

    Bug addressed which is exposed if workbook contains a revision page.  Just like the .XLS parser, this application incorporates the sheet_valid flag, and thus won't processes the BOM unless it has been determined that the sheet is valid.  The issue related to opened file types has been corrected.  Now, only .xlsx files are opened and parsed.

--- Combined_BOM.py
+++ Combined_BOM.py
@@ -3,8 +3,8 @@ FILE: Combined_BOM.py
 
 PURPOSE: 
 When running this script, the user will be able to create one large flat BOM
-for purchasing purposes.  This script will only operate on .xls files, and not
-.xlsx files.  This script will automatically sift through all files in the 
+for purchasing purposes.  This script will only operate on .xlsx files, and not
+.xls files.  This script will automatically sift through all files in the 
 current working directory, and with each file, it will iterate over all sheets. 
 
 AUTHOR: 
@@ -35,12 +35,12 @@ CR1_col		= 0						# Column location for supplier name
 CR1PN_col	= 0						# Column location for supplier's PN
 NOTE_col 	= 0 					# Column location for "notes" field
 BOM_HEADER 	= ["QPN","QTY","DES","MFG","MFGPN","CR1","CR1PN","NOTES"]
-EXTS		= ('.xls','.xlsx')		#Supported file extensions
 
 MAX_HITS = 5			#This many hits will trigger us to leave the searching loop
 data_start = 0			#This is the row where the data starts
 
 search_header 	= []		# Set equal to BOM_HEADER and pop elements until we find all the colums we're looking for
+sheet_valid		= False		# Flag that tells application if a sheet contains valid data or not
 header 			= []		# This array will define the column locations for the header
 qpn 			= []        # Pull in all QPNs into a list. This will make them easier to work with later
 asso 			= []       	# Pull in all associations into a list. This will make them easier to work with later
@@ -99,7 +99,7 @@ if __name__ == '__main__':
 	for i in range(len(files)-1):
 		
 		# Search through and open files that are appended with *.xlsx
-		if(files[i].lower().endswith(ext) for ext in EXTS):
+		if(files[i].lower().endswith(".xlsx")):
 			
 			print "Opening file: ", files[i]
 			wb = load_workbook(filename = files[i])     #Open the workbook that we are going to parse though 
@@ -115,6 +115,7 @@ if __name__ == '__main__':
 			
 			# Iterate through all sheets in the opened file
 			for sh in range (num_sheets):
+				sheet_valid = False
 				current_sheet = wb.get_sheet_by_name(name = ws[sh])
 				association = raw_input("Enter a unique association / high-level QPN for this worksheet (i.e. Prog Cbl): ") 
 				
@@ -150,22 +151,23 @@ if __name__ == '__main__':
 							search_header.remove("DES")
 						
 						elif((temptext.find("Qty")!=-1) or (temptext.find("QTY")!=-1)):		#Look for Quantity field.  
-							search_header.remove("QTY")
 							QTY_col = c
+							search_header.remove("QTY")
 						
 						elif( ((temptext.find("cr1")!=-1) or (temptext.find("CR1")!=-1)) and (len(temptext) <= 4) ):		#Look for CR1, and cannot have PN as in CR1PN
-							search_header.remove("CR1")
 							CR1_col = c
+							search_header.remove("CR1")
 						
 						elif( ((temptext.find("cr1pn")!=-1) or (temptext.find("CR1PN")!=-1)) and (len(temptext) > 4) ):		#Look for CR1PN
-							search_header.remove("CR1PN")
 							CR1PN_col = c
+							search_header.remove("CR1PN")
 						
 						elif((temptext.find("notes")!=-1) or (temptext.find("NOTES")!=-1) or (temptext.find("Notes")!=-1) or (temptext.find("Note")!=-1) or (temptext.find("note")!=-1) ):		#Look for Notes 
-							search_header.remove("NOTES")
 							NOTE_col = c
+							search_header.remove("NOTES")
 					
 					if( (len(search_header) == 0) ):		# Found all header fields
+						sheet_valid = True
 						data_start = r + 1			# Plenty of confidence at this point that we've found data start
 						print 'Data appears to start on row: ', data_start
 						
@@ -176,83 +178,89 @@ if __name__ == '__main__':
 							)
 						break
 					
-					elif( (r == 10) and (len(search_header) > 0) ):		# Some header fields are missing, so shutdown
+					elif( (r == 10) and (len(search_header) > 0) and sh >= num_sheets ):		# Some header fields are missing, so shutdown
 						print "On sheet: " + str(current_sheet) + " -- did not find headers: ", search_header
 						sys.exit(0)
-				
-				print "QPN column found to be: " + str(QPN_col)		
-				print "QTY column found to be: " + str(QTY_col)
-				print "Description column found to be: " + str(DES_col)		
-				print "MFG column found to be: " + str(MFG_col)
-				print "MFGPN column found to be: " + str(MFGPN_col)
-				print "CR1 column found to be: " + str(CR1_col)
-				print "CR1PN column found to be: " + str(CR1PN_col)
-				print "NOTES column found to be: " + str(NOTE_col)
-				
-				header = [0,QPN_col,DES_col,MFG_col,MFGPN_col,CR1_col,CR1PN_col,QTY_col,NOTE_col]
-				header_values = ["Association","QPN","DES","MFG","MFGPN","CR1","CR1PN","QTY","NOTES"]
-				
-				# Now iterate through all rows of the current sheet and populate the data lists
-				blank_row_count = 0		# Reset number of blank rows detected.  When three in a row are detected, break out of the loop. 
-				for r in range (data_start,num_rows + 1):
-					
-					
-					# If multiple columns are blank, break out of this loop for these are empty cells
-					if( (len(clean_value(unicode(current_sheet.cell(row = r, column=QPN_col).value))) <= 1) and ( len(clean_des(unicode(current_sheet.cell(row = r, column=DES_col).value))) <= 1) and
-						( len(clean_value(unicode(current_sheet.cell(row = r, column=MFG_col).value))) <= 1) ):
-						blank_row_count += 1				# Increase value of blank row count
-						print 'Blank row detected at row (', r, ')'
-					else:
-						blank_row_count = 0					
-						asso.append(association)				#For each row in the BOM, we need to append the association
-						print( 	'Sample data, current row: ', clean_value(unicode(current_sheet.cell(row = r, column=QPN_col).value)),' ', 
-								clean_value(unicode(current_sheet.cell(row = r, column=DES_col).value)), ' ', 
-								clean_value(unicode(current_sheet.cell(row = r, column=MFG_col).value)), ' ', 
-								clean_value(unicode(current_sheet.cell(row = r, column=MFGPN_col).value))
-							)
-						
-						current_value = clean_value(unicode(current_sheet.cell(row = r, column=QPN_col).value))
-						if current_value == "None":
-							current_value = ""
-						qpn.append(current_value)			
-						
-						current_value = clean_value(unicode(current_sheet.cell(row = r, column=DES_col).value))
-						if current_value == "None":
-							current_value = ""
-						des.append(current_value)
-						
-						current_value = clean_value(unicode(current_sheet.cell(row = r, column=MFG_col).value))
-						if current_value == "None":
-							current_value = ""
-						mfg.append(current_value)
-						
-						current_value = clean_value(unicode(current_sheet.cell(row = r, column=MFGPN_col).value))
-						if current_value == "None":
-							current_value = ""
-						mfgpn.append(current_value)
-						
-						current_value = clean_value(unicode(current_sheet.cell(row = r, column=CR1_col).value))
-						if current_value == "None":
-							current_value = ""
-						cr1.append(current_value)
-						
-						current_value = clean_value(unicode(current_sheet.cell(row = r, column=CR1PN_col).value))
-						if current_value == "None":
-							current_value = ""
-						cr1pn.append(current_value)
-						
-						current_value = clean_value(unicode(current_sheet.cell(row = r, column=QTY_col).value))
-						if current_value == "None":
-							current_value = ""
-						qty.append(current_value)
-						
-						current_value = clean_value(unicode(current_sheet.cell(row = r, column=NOTE_col).value))
-						if current_value == "None":
-							current_value = ""
-						notes.append(current_value)
 
-					if(blank_row_count >= 3):
-						break								# Too many blank rows detected, so break out of the loop.  
+					elif((r == 10) and (len(search_header) > 0) and sh < num_sheets):
+						sheet_valid = False
+						print "* File: ", str(files[i]), "Invalid Sheet: " + str(worksheets[sh]) + " -- did not find headers: ", search_header
+						break
+
+				if(sheet_valid):
+					print "QPN column found to be: " + str(QPN_col)		
+					print "QTY column found to be: " + str(QTY_col)
+					print "Description column found to be: " + str(DES_col)		
+					print "MFG column found to be: " + str(MFG_col)
+					print "MFGPN column found to be: " + str(MFGPN_col)
+					print "CR1 column found to be: " + str(CR1_col)
+					print "CR1PN column found to be: " + str(CR1PN_col)
+					print "NOTES column found to be: " + str(NOTE_col)
+					
+					header = [0,QPN_col,DES_col,MFG_col,MFGPN_col,CR1_col,CR1PN_col,QTY_col,NOTE_col]
+					header_values = ["Association","QPN","DES","MFG","MFGPN","CR1","CR1PN","QTY","NOTES"]
+					
+					# Now iterate through all rows of the current sheet and populate the data lists
+					blank_row_count = 0		# Reset number of blank rows detected.  When three in a row are detected, break out of the loop. 
+					for r in range (data_start,num_rows + 1):
+						
+						
+						# If multiple columns are blank, break out of this loop for these are empty cells
+						if( (len(clean_value(unicode(current_sheet.cell(row = r, column=QPN_col).value))) <= 1) and ( len(clean_des(unicode(current_sheet.cell(row = r, column=DES_col).value))) <= 1) and
+							( len(clean_value(unicode(current_sheet.cell(row = r, column=MFG_col).value))) <= 1) ):
+							blank_row_count += 1				# Increase value of blank row count
+							print 'Blank row detected at row (', r, ')'
+						else:
+							blank_row_count = 0					
+							asso.append(association)				#For each row in the BOM, we need to append the association
+							print( 	'Sample data, current row: ', clean_value(unicode(current_sheet.cell(row = r, column=QPN_col).value)),' ', 
+									clean_value(unicode(current_sheet.cell(row = r, column=DES_col).value)), ' ', 
+									clean_value(unicode(current_sheet.cell(row = r, column=MFG_col).value)), ' ', 
+									clean_value(unicode(current_sheet.cell(row = r, column=MFGPN_col).value))
+								)
+							
+							current_value = clean_value(unicode(current_sheet.cell(row = r, column=QPN_col).value))
+							if current_value == "None":
+								current_value = ""
+							qpn.append(current_value)			
+							
+							current_value = clean_value(unicode(current_sheet.cell(row = r, column=DES_col).value))
+							if current_value == "None":
+								current_value = ""
+							des.append(current_value)
+							
+							current_value = clean_value(unicode(current_sheet.cell(row = r, column=MFG_col).value))
+							if current_value == "None":
+								current_value = ""
+							mfg.append(current_value)
+							
+							current_value = clean_value(unicode(current_sheet.cell(row = r, column=MFGPN_col).value))
+							if current_value == "None":
+								current_value = ""
+							mfgpn.append(current_value)
+							
+							current_value = clean_value(unicode(current_sheet.cell(row = r, column=CR1_col).value))
+							if current_value == "None":
+								current_value = ""
+							cr1.append(current_value)
+							
+							current_value = clean_value(unicode(current_sheet.cell(row = r, column=CR1PN_col).value))
+							if current_value == "None":
+								current_value = ""
+							cr1pn.append(current_value)
+							
+							current_value = clean_value(unicode(current_sheet.cell(row = r, column=QTY_col).value))
+							if current_value == "None":
+								current_value = ""
+							qty.append(current_value)
+							
+							current_value = clean_value(unicode(current_sheet.cell(row = r, column=NOTE_col).value))
+							if current_value == "None":
+								current_value = ""
+							notes.append(current_value)
+
+						if(blank_row_count >= 3):
+							break								# Too many blank rows detected, so break out of the loop.  
 					
 
 	print "\n+++++++++++++++++++++++++++++++++++++++++++++++"
--- README.md
+++ README.md
@@ -8,3 +8,5 @@ When running this script, the user will be able to create one large flat BOM for
 v1.0 -- Initial (tested) release.  It's worth noting that this version works with unicode, and so special symbols, if encountered, shouldn't cause a crash.  
 
 v1.1 -- No longer are internal white spaces removed from descriptions, notes, etc., but rather only those that are leading or trailing.  This prevents descriptions, notes, etc. from being run together.
+
+v1.2 -- Bug addressed which is exposed if workbook contains a revision page.  Just like the .XLS parser, this application incorporates the "sheet_valid" flag, and thus won't processes the BOM unless it has been determined that the sheet is valid.  The issue related to opened file types has been corrected.  Now, only .xlsx files are opened and parsed.  
